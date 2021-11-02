b553243e58b075560467da3c75479ed60397f91d
Svitlana Hovorova <cybermavka@Svitlanas-MacBook-Air.local>
2021-11-03 00:26:07 +0200

    Final with cheat documentation

--- target_ua.py
+++ target_ua.py
@@ -6,22 +6,27 @@ from random import choice
 def generate_grid():
     """
     generates grid for game
+    >>> print('a')
+    a
     """
     grid = []
-    letters = 'йцукенгшщзхїґфивапролджєячсмітьбю'
+    letters =  ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н',
+    'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я', 'є', 'і', 'ї', 'ґ']
     while len(grid) < 5:
         letter = choice(letters)
         if letter not in grid:
             grid.append(letter)
     return grid
 
-def get_words(f, letters):
+def get_words(fpath, letters):
     """
     gets words from dictionary for game
+    >>> print('a')
+    a
     """
     parts_of_speech = ['/n', 'noun', '/v', 'verb', '/adj', 'adj', 'adv']
     words = []
-    with open(f, 'r') as output_file:
+    with open(fpath, 'r') as output_file:
         all_of_it = output_file.read()
     all_lines = list(all_of_it.split('\n'))
     for ch_line in all_lines:
@@ -39,12 +44,16 @@ def get_words(f, letters):
                 if par in buff[1]:
                     if par == '/n' or par == 'noun':
                         words_part = 'noun'
-                    if par == '/v' or par == 'verb':
+                        break
+                    elif par == '/v' or par == 'verb':
                         words_part = 'verb'
-                    if par == '/adj' or par == 'adj':
+                        break
+                    elif par == '/adj' or par == 'adj':
                         words_part = 'adjective'
-                    if par == 'adv':
+                        break
+                    elif par == 'adv':
                         words_part = 'adverb'
+                        break
             if words_word and words_part:
                 words.append((words_word, words_part))
     return words
@@ -52,6 +61,8 @@ def get_words(f, letters):
 def check_user_words(user_words, language_part, letters, dict_of_words):
     """
     Checks user words and returns game results
+    >>> print('a')
+    a
     """
     part_dict_words = []
     user_correct = []
